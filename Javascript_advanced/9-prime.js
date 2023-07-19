ce1a74c996622d11d2f1b868c524ef4baf4129eb
kkeas <keaskatrina@gmail.com>
2023-07-19 00:22:25 -0500

    9

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,24 @@
+function countPrimeNumbers() {
+  let primes = 0;
+  for (let i = 2; i <= 100; i++) {
+      if (isPrime(i)) {
+          primes++;
+      }
+  }
+  return primes;
+}
+
+function isPrime(num) {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+      if (num % i === 0) {
+          return false;
+      }
+  }
+  return num > 1;
+}
+
+const t0 = performance.now();
+console.log(countPrimeNumbers());
+const t1 = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
